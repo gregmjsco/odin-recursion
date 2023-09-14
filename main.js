358422e6f64bd9fb3b0075e796278bdeb7a3fb21
Greg Johnston <gregmj.sco@gmail.com>
2023-09-14 23:55:31 +0900

    Add non recursive solution to Fibonacci Sequence problem

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Test")
+
+function fib(n) {
+    let result = [0];
+
+    for (let i = 1; result.length < n; i++) {
+        result.push(i + result[i - 1])
+    }
+    console.log("Result: " + result)
+}
+
+fib(8);
