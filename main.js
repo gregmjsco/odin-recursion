bcc3cd7b574d1f8244ca2b50eff76ea985cd1bf4
Greg Johnston <gregmj.sco@gmail.com>
2023-09-15 20:48:31 +0900

    Add working recursive solution

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ function fib(n) {
 }
 
 fib(8);
+
+
+function fibRec(n, result = [0, 1]) {
+    if (n <= 0) {
+        return [];
+    }
+
+    if (result.length >= n) {
+        return result.slice(0, n);
+    }
+
+    result.push((result[result.length - 1] + result[result.length - 2]))
+    return fibRec(n, result)
+}
+
+console.log(fibRec(8))
